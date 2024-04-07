f421621f2252024911a784f716f0c8f43d807a4b
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2024-04-07 00:32:56 -0300

    styling Resposta component

--- components/Questao.tsx
+++ components/Questao.tsx
@@ -3,6 +3,13 @@ import styles from '../styles/Questao.module.css';
 import Enunciado from "./Enunciado";
 import Resposta from "./Resposta";
 
+const letras = [
+  { valor: 'A', cor: '#F2C866' },
+  { valor: 'B', cor: '#F266BA' },
+  { valor: 'C', cor: '#85D4F2' },
+  { valor: 'D', cor: '#BCE596' },
+]
+
 interface QuestaoProps {
   valor: QuestaoModel
 }
@@ -16,8 +23,8 @@ const Questao = (props: QuestaoProps) => {
           <Resposta key={i}
           valor={resposta}
           indice={i}
-          letra="A"
-          corLetra="#F2C866"
+          letra={letras[i].valor}
+          corFundoLetra={letras[i].cor}
         />
       )
     })
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import QuestaoModel from "@/model/questao";
 import RespostaModel from "@/model/resposta";
 
 const Home = () => {
-  const questaoTeste = new QuestaoModel(1, 'Melhor cor?', [
+  const questaoTeste = new QuestaoModel(1, 'Qual a melhor cor?', [
     RespostaModel.errada('Vermelha'),
     RespostaModel.errada('Preto'),
     RespostaModel.errada('Azul'),
