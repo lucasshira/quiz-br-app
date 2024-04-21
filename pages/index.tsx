9a4c969a0b5c3e674613663702d958aaafba9d90
Lucas Shirabayashi Azevedo <144144880+lucasshira@users.noreply.github.com>
2024-04-21 00:20:56 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import QuestaoModel from "@/model/questao";
 import { useEffect, useState } from "react";
 import { useRouter } from 'next/router';
 
-const BASE_URL = 'https://quiz-br-app.vercel.app/';
+const BASE_URL = 'https://quiz-br-app.vercel.app/api';
 
 const Home = () => {
   const router = useRouter();
